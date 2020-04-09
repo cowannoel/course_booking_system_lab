58dfe9c5af5ccbc40e0ff17b08c53c58a2b36684
cowannoel <cowannoel@gmail.com>
2020-04-09 17:22:33 +0100

    end of lab mk2

--- src/main/java/com/codeclan/example/courseBookingSystem/repositories/CustomerRepository.java
+++ src/main/java/com/codeclan/example/courseBookingSystem/repositories/CustomerRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    List<Customer> findAllBookingsByName(String name);
+    List<Customer> findAllByBookingsCourseId(Long courseId);
 
 }
